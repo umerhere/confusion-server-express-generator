fbdd8f8a96f2374c31c6039800878ee47d6baaa8
Umar Aslam <umar.aslam@RT-LT-606.rolustech.net>
2022-08-15 15:46:05 +0500

    connect mongo server with APP using mongoose

--- app.js
+++ app.js
@@ -11,6 +11,18 @@ const dishRouter = require('./routes/dishRouter');
 const promoRouter = require('./routes/promoRouter');
 const leaderRouter = require('./routes/leaderRouter');
 
+const mongoose = require('mongoose');
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/confusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+  console.log("Connected to DB Server");
+}, (err) => {
+  console.log(err);
+});
+
 var app = express();
 
 // view engine setup
---
+++ models/dishes.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        require: true,
+    },
+    comment: {
+        type: String,
+        require: true
+    },
+    author: {
+        type: String,
+        require: true
+    },
+}, {
+    timestamps: true
+});
+
+const dishSchema = new Schema({
+    name: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        require: true
+    },
+    comments: [commentSchema]
+}, {
+    timestamps: true
+});
+
+var Dishes = mongoose.model('Dish', dishSchema);
+module.exports = Dishes
